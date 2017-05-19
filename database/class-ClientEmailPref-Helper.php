99a19fac17f3c12f8c0049b9ca574b1dcba3fd98
Ronij Pandey <ronij.2508@gmail.com>
2017-05-19 22:28:13 +0530

    #209 Update class-ClientEmailPref-Helper

--- database/class-ClientEmailPref-Helper.php
+++ database/class-ClientEmailPref-Helper.php
@@ -6,8 +6,9 @@ class ClientEmailPref_Helper
 	{
 		global $wpdb;
 		$resultEmailIds = array();
+		$users_table = $wpdb->prefix.'users';
 		$table_name = $wpdb->prefix . 'edugorilla_client_preferences';
-		$client_email_addresses = $wpdb->get_results("SELECT * FROM $table_name");
+		$client_email_addresses = $wpdb->get_results("SELECT ut.display_name AS client_name,ut.user_email AS email_id,cpt.* FROM $table_name cpt,$users_table ut WHERE ut.ID=cpt.id");
 		foreach ($client_email_addresses as $cea) {
 			if (in_array($cea->email_id, $filterEmailIds)) {
 				if ($cea->unsubscribe_email != 0) {
