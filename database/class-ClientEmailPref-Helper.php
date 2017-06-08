c6e835d528ede4d6eb3ca4fcee6f2d94010d2830
Rohit Manglik <rohit.manglik@gmail.com>
2017-06-08 11:19:48 +0530

    Corrected usage of users table

--- database/class-ClientEmailPref-Helper.php
+++ database/class-ClientEmailPref-Helper.php
@@ -6,7 +6,7 @@ class ClientEmailPref_Helper
 	{
 		global $wpdb;
 		$resultEmailIds = array();
-		$users_table = $wpdb->prefix.'users';
+		$users_table = $wpdb->users;
 		$table_name = $wpdb->prefix . 'edugorilla_client_preferences';
 		$client_email_addresses = $wpdb->get_results("SELECT ut.display_name AS client_name,ut.user_email AS email_id,cpt.* FROM $table_name cpt,$users_table ut WHERE ut.ID=cpt.id");
 		foreach ($client_email_addresses as $cea) {
