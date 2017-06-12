cc232dcf12ac936ee197ebbb46576e5b1620f8f6
Anantharam <anantharam@live.com>
2017-06-13 01:34:29 +0530

    Refactoring filter button

--- js/backend_clientlib.js
+++ js/backend_clientlib.js
@@ -72,116 +72,117 @@ $(document).on('click','a[id^=edugorilla_leads_view]', function(){
 	});
 
 $(document).on('click','#edugorilla_filter',function(){
+	validateFormEntries();
+	prepopulateSaveButton();
+});
+
+	var validateFormEntries = function () {
+		var eduName = $("#edu_name").val();
+		var eduEmail = $("#edu_email").val();
+		var eduContactNo = $("#edu_contact_no").val();
+		var eduQuery = $("#edu_query").val();
+		var eduInstituteDatas = $("#edugorilla_institute_datas").val();
+		var eduSubscriptionDatas = $("#edu_email").val();
+		if (eduName !== "" && (eduEmail !== "" || eduContactNo !== "") && eduQuery !== "") {
+			if (eduInstituteDatas !== "" || eduSubscriptionDatas !== "") {
+				$('#save_details_button').removeAttr("disabled");
+				//We should be showing modal in any case.
+				// if ($("#is_promotional_lead").is(":checked")) {
+				$('#save_details_button').attr("rel", "modal:open");
+				// } else {
+				//    $('#save_details_button').attr("onclick", "document.lead_capture_details.submit();");
+				// }
+			}
+		} else {
+			$('#save_details_button').attr('disabled', true);
+		}
+	};
+
+	var prepopulateSaveButton = function () {
 		var institute_data;
-		     
+
 		var ptype = $("#edugorilla_listing_type").val();
 		var keyword = $('#edugorilla_keyword').val();
 		var location = $('#edugorilla_location').val();
 		var category = $("#edugorilla_category").val();
-	var categoryString = JSON.stringify(category);
-		
-    		$.ajax({
-            	url: ajaxurl,
-                type: 'GET',
-         		data: {
-            			'action':'edugorilla_show_location',
-            			'ptype': ptype,
-                		'term': keyword,
-                        'address' : location,
-		            'category': categoryString
-          			  },
-                dataType: 'json',
-                beforeSend: function()
-            	{
-                },
-         		success: function(data) 
-            	{
-		            $("#edugorilla_institute_datas").val(JSON.stringify(data['postingDetails']));
-		            $("#edugorilla_subscibed_instant_datas").val(JSON.stringify(data['subscriptionPreferenceDetails']));
+		var categoryString = JSON.stringify(category);
 
-		            var eduName = $("#edu_name").val();
-		            var eduEmail = $("#edu_email").val();
-		            var eduContactNo = $("#edu_contact_no").val();
-		            var eduQuery = $("#edu_query").val();
-		            var eduInstituteDatas = $("#edugorilla_institute_datas").val();
-		            var eduSubscriptionDatas = $("#edu_email").val();
-		            if (eduName !== "" && (eduEmail !== "" || eduContactNo !== "") && eduQuery !== "")
-                    {
-	                    if (eduInstituteDatas !== "" || eduSubscriptionDatas !== "") {
-		                    $('#save_details_button').removeAttr("disabled");
-		                    //We should be showing modal in any case.
-		                    // if ($("#is_promotional_lead").is(":checked")) {
-		                    $('#save_details_button').attr("rel", "modal:open");
-		                    // } else {
-		                    //    $('#save_details_button').attr("onclick", "document.lead_capture_details.submit();");
-		                    // }
-	                    }
-                    }
+		$.ajax({
+			url: ajaxurl,
+			type: 'GET',
+			data: {
+				'action': 'edugorilla_show_location',
+				'ptype': ptype,
+				'term': keyword,
+				'address': location,
+				'category': categoryString
+			},
+			dataType: 'json',
+			beforeSend: function () {
+			},
+			success: function (data) {
+				$("#edugorilla_institute_datas").val(JSON.stringify(data['postingDetails']));
+				$("#edugorilla_subscibed_instant_datas").val(JSON.stringify(data['subscriptionPreferenceDetails']));
 
-                var address = $("#edugorilla_location option:selected").text().replace("->","");
-              	
-                var geocoder =  new google.maps.Geocoder();
-               
-    geocoder.geocode( { 'address': address}, function(results, status) {
-       
-          	var points;
-          	var zoom;
-  				if(typeof results[0] === "undefined")
-                {
-                	points = {lat: parseFloat(0), lng: parseFloat(0)};
-                	zoom = 1;
-                }
-    			else
-                {
-                	points = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()};
-                	zoom = 5;
-                }
+				var address = $("#edugorilla_location option:selected").text().replace("->", "");
+
+				var geocoder = new google.maps.Geocoder();
+
+				geocoder.geocode({'address': address}, function (results, status) {
+
+					var points;
+					var zoom;
+					if (typeof results[0] === "undefined") {
+						points = {lat: parseFloat(0), lng: parseFloat(0)};
+						zoom = 1;
+					}
+					else {
+						points = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()};
+						zoom = 5;
+					}
 
 
-	    var cnfbox = "<table class='widefat fixed' align='center' width='100%' border=1><tr><th>Send</th><th>Institude Name</th><th>Email(s)</th><th>SMS(s)</th><th>Flag</th></tr>";
-        			 	var map = new google.maps.Map(document.getElementById('map'), {
-         					 zoom: zoom,
-        				 	center: points
-       				 	});
-                
-                 var infowindow = new google.maps.InfoWindow();
+					var cnfbox = "<table class='widefat fixed' align='center' width='100%' border=1><tr><th>Send</th><th>Institude Name</th><th>Email(s)</th><th>SMS(s)</th><th>Flag</th></tr>";
+					var map = new google.maps.Map(document.getElementById('map'), {
+						zoom: zoom,
+						center: points
+					});
 
-	    if ($("#is_promotional_lead").is(":checked")) {
-		    $.each(data['postingDetails'], function (i, v) {
+					var infowindow = new google.maps.InfoWindow();
 
-			    cnfbox += "<tr id=" + v.post_id + "><td><input type='checkbox' class='confirmSendPostingDetails' checked='true'/></td><td><a href='" + v.listing_url + "'>" + v.title + "</a></td><td>" + v.emails + "</td><td>" + v.phones + "</td><td>" + v.flag + "</td></tr>";
+					if ($("#is_promotional_lead").is(":checked")) {
+						$.each(data['postingDetails'], function (i, v) {
 
-			    var marker = new google.maps.Marker({
-				    position: new google.maps.LatLng(v.lat, v.long),
-				    map: map
-			    });
-			    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-				    return function () {
-					    infowindow.setContent('Institute Name: <b><a href="' + v.listing_url + '">' + v.title + "</a></b><br>Address: <b>" + v.address + "</b><br>Latitude <b>" + v.lat + "</b><br>Longitude <b>" + v.long + "</b><br>Flag <b>" + v.flag + "</b>");
-					    infowindow.open(map, marker);
-				    }
-			    })(marker, i));
-		    });
-	    }
+							cnfbox += "<tr id=" + v.post_id + "><td><input type='checkbox' class='confirmSendPostingDetails' checked='true'/></td><td><a href='" + v.listing_url + "'>" + v.title + "</a></td><td>" + v.emails + "</td><td>" + v.phones + "</td><td>" + v.flag + "</td></tr>";
 
-	    $.each(data['subscriptionPreferenceDetails'], function (i, v) {
-		    cnfbox += "<tr id=" + v.userId + "><td><input type='checkbox' class='confirmSendPrefDetails' checked='true'/></td><td>" + v.userName + "</td><td class='emailPrefDetails'>" + v.emailDetails + "</td><td class='phonePrefDetails'>" + v.phoneDetails + "</td></tr>";
-	    });
+							var marker = new google.maps.Marker({
+								position: new google.maps.LatLng(v.lat, v.long),
+								map: map
+							});
+							google.maps.event.addListener(marker, 'click', (function (marker, i) {
+								return function () {
+									infowindow.setContent('Institute Name: <b><a href="' + v.listing_url + '">' + v.title + "</a></b><br>Address: <b>" + v.address + "</b><br>Latitude <b>" + v.lat + "</b><br>Longitude <b>" + v.long + "</b><br>Flag <b>" + v.flag + "</b>");
+									infowindow.open(map, marker);
+								}
+							})(marker, i));
+						});
+					}
 
-	    cnfbox += "</table><center><button id='confirm' onclick='document.lead_capture_details.submit();'>Confirm</button></center>";
-               		$("#confirmation").html(cnfbox);
- 
-        });
+					$.each(data['subscriptionPreferenceDetails'], function (i, v) {
+						cnfbox += "<tr id=" + v.userId + "><td><input type='checkbox' class='confirmSendPrefDetails' checked='true'/></td><td>" + v.userName + "</td><td class='emailPrefDetails'>" + v.emailDetails + "</td><td class='phonePrefDetails'>" + v.phoneDetails + "</td></tr>";
+					});
 
-	            },
-                error: function(err)
-            	{
-                	console.log(err);
-                }
-            });
-					 
-});
+					cnfbox += "</table><center><button id='confirm' onclick='document.lead_capture_details.submit();'>Confirm</button></center>";
+					$("#confirmation").html(cnfbox);
 
+				});
+
+			},
+			error: function (err) {
+				console.log(err);
+			}
+		});
+	};
 
 	$('#edugorilla_category').select2({placeholder: 'Select category'});
 	$('#edugorilla_location').select2();
